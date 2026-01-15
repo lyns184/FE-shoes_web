1cceb05975d224a849f34f42d7c1f6ed9f0c8cf4
lyns <khanhlinh.lepham@gmail.com>
2026-01-15 19:18:07 +0700

    save

--- .history/src/App_20260101171816.tsx
+++
@@ -1,8 +0,0 @@
-import NotFoundPage from './pages/NotFound'
-import './App.css'
-
-function App() {
-  return <NotFoundPage />
-}
-
-export default App
--- .history/src/App_20260101172051.tsx
+++
@@ -1,8 +0,0 @@
-import SignUp from './pages/SignUp'
-import './App.css'
-
-function App() {
-  return <SignUp />
-}
-
-export default App
